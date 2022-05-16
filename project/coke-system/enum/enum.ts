b4e54b3679cf272fcd3c3afeb24c763a5439a6bc
PomeloCoke <keyueyuan@gmail.com>
2022-05-16 22:40:32 +0800

    practice

---
+++ project/coke-system/enum/enum.ts
@@ -0,0 +1,8 @@
+enum DAY { Today, Yesterday, Tomorrow, DBY, DAT }
+enum WEEK { Mon, Tue, Wed, Thu, Fri, Sat, Sun, LastW, ThisW, NextW }
+enum MONTH { Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sept, Oct, Nov, Dec, LastM, ThisM, NextM }
+enum QUATER { Q1, Q2, Q3, Q4, LastQ, ThisQ, NextQ }
+
+enum RES_STATUS {
+
+}
