aa6440b4dd536341141caa12ced78575fc7d751f
PomeloCoke <keyueyuan@gmail.com>
2022-05-17 22:40:00 +0800

    js class practice

--- demo/components/native/utils/index.js
+++ demo/components/native/utils/index.js
@@ -1,3 +1,25 @@
+const methodPrefix = 'test'
 class COKE_UI {
-  
+  constructor() {
+    this._year = 2017
+    this.edition = 1
+  }
+  [methodPrefix + 'A']() {
+    // console.log('getFunc', methodPrefix + 'A')
+  }
+  get year() {
+    return this._year
+  }
+  set year(newVal) {
+    if (newVal > 2017) {
+      this._year = newVal
+      this.edition += newVal - 2017
+    }
+  }
 }
+
+const testClass = new COKE_UI()
+
+console.log(testClass.year)
+testClass.year = 2018
+console.log(testClass.edition)
