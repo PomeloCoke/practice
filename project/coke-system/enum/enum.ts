2ecb5a59c0a97e8f3308403d167d48157e4c265e
PomeloCoke <keyueyuan@gmail.com>
2022-06-10 07:46:23 +0800

    近期练习

--- project/coke-system/enum/enum.ts
+++ project/coke-system/enum/enum.ts
@@ -6,3 +6,8 @@ enum QUATER { Q1, Q2, Q3, Q4, LastQ, ThisQ, NextQ }
 enum RES_STATUS {
 
 }
+
+enum OS { Windows, Mac, Android, Ios, Other }
+enum DEVICE { PC, Mobile, Pad, Watch, Glass, Other }
+enum BROWSER { Chrome, Firefox, IE, Edge, Safari, Xiaomi, UC }
+
---
+++ project/hello-world/hw-system/index.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+
+const content = 'some content'
+
+// fs.writeFile('test.txt', content, err=> {
+//   console.log('helloWorld', err)
+// })
+
+// fs.readFile('./test.json', 'utf8', (err, data) => {
+//   console.log('helloWorld', JSON.parse(data))
+// })
+console.log()
