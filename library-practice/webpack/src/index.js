d8a9a97ee960278931ce70710558eaa10216ed9b
PomeloCoke <keyueyuan@gmail.com>
2022-06-10 08:23:43 +0800

    webpack练习

--- library-practice/webpack/src/index.js
+++ library-practice/webpack/src/index.js
@@ -1,8 +1,8 @@
-import _ from 'lodash'
+
 function component() {
   const element = document.createElement('div')
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+  element.innerHTML = 'Hello World'
 
   return element
 }
