7ac19cb13c0d1e566c5831f62adbd01604e5426b
PomeloCoke <keyueyuan@gmail.com>
2022-06-12 21:22:58 +0800

    webpack+react练习

--- project/hello-world/hw-system/index.js
+++ project/hello-world/hw-system/index.js
@@ -5,12 +5,13 @@ const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'src/app.js')
-    }
+    // webPreferences: {
+    //   preload: path.join(__dirname, 'src/app.js')
+    // }
   })
 
-  win.loadFile('./dist/index.html')
+  // win.loadFile('http://localhost:3000')
+  win.loadURL('http://localhost:3000')
 }
 
 app.whenReady().then(() => {
