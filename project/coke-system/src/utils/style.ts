e3ed5c3c0bb33b134e6a7ff02df01ed3e7e18ff3
PomeloCoke <keyueyuan@gmail.com>
2023-01-04 00:01:02 +0800

    container practice

--- project/coke-system/src/utils/style.ts
+++ project/coke-system/src/utils/style.ts
@@ -1,4 +1,11 @@
 window.className = function className(styleList: string[]): string {
   return styleList.filter(item=>item).join(" ")
 }
+
+const getWinHeight = () => {
+  let height =
+    window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight || 0
+
+  return height
+}
 export default {}
--- project/coke-system/src/views/single/login/index.tsx
+++ project/coke-system/src/views/single/login/index.tsx
@@ -5,13 +5,20 @@ import useStore from '@/stores';
 import Styles from './index.module.less'
 import IconFont from "@/components/iconfont";
 
+const getBgStyle = () => {
+
+}
+
 function Login() {
   
 
   return (
     <div className={Styles.page__single__login}>
       <IconFont className={Styles.logo} name="icon-logo" />
-      
+      <div className={Styles.slot__left}></div>
+      <div className={Styles.slot__right}>
+        <img src="https://pics.pomelode.com/admin/login-bg.png" alt="" className={Styles.background} />
+      </div>
     </div>
   )
 }
